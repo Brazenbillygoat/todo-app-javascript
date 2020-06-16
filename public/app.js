d2a564d53cfaa0d13cf94f9ffc27524ca644fdb0
Hyrum Butler <hyrum.butler@students.svu.edu>
2020-06-15 21:05:38 -0400

    Able to add tasks to the homepage with immediate refresh

--- public/app.js
+++ public/app.js
@@ -1,25 +1,43 @@
 
-const itemList = document.getElementById('todo-items')
-const text = document.createTextNode('goodbye')
+// const itemList = document.getElementById('todo-items')
 
-console.log("hello")
-itemList.addEventListener("load", () => {
-    console.log(text)
-    console.log("goodbye")
-    createItem()
-})
 
-function createItem() {
-    console.log('maybe')
-}
-console.log("hello")
-// paragraph.appendChild(text) = "testing"
-// const body = document.getElementById('body')
-// body.appendChild(paragraph)
+// console.log(dbItems)
+// console.log("hello")
+// window.addEventListener("load", () => {
 
-// var para = document.createElement("p");
-// var node = document.createTextNode("This is new.");
-// para.appendChild(node);
+//     createItem()
+// })
 
-// var element = document.getElementById("test");
-// element.appendChild(para);
+// function createItem() {
+//     let readItems = 
+//         `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+//         <span class="item-text">${global.dbItems}</span>
+//         <div>
+//           <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+//           <button class="delete-me btn btn-danger btn-sm">Delete</button>
+//         </div>
+//       </li>`;
+//     itemList.insertAdjacentHTML('beforeend', readItems)
+// }
+
+// // paragraph.appendChild(text) = "testing"
+// // const body = document.getElementById('body')
+// // body.appendChild(paragraph)
+
+// // var para = document.createElement("p");
+// // var node = document.createTextNode("This is new.");
+// // para.appendChild(node);
+
+// // var element = document.getElementById("test");
+// // element.appendChild(para);
+
+// // `${items.map((item) => {
+// //     return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+// //     <span class="item-text">${item.text}</span>
+// //     <div>
+// //       <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+// //       <button class="delete-me btn btn-danger btn-sm">Delete</button>
+// //     </div>
+// //   </li>`
+// //   }).join('')}`
