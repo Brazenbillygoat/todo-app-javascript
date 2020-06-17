ff86dbcea0ca8c52f1b40a3945a363ccd8af8e33
Hyrum Butler <hyrum.butler@students.svu.edu>
2020-06-16 20:56:11 -0400

    cleaning up for deployment

--- public/app.js
+++ public/app.js
@@ -1,8 +1,4 @@
 
-// const itemList = document.getElementById('todo-items')
-
-//const e = require("express")
-
 function itemTemplate(item) {
     return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
     <span class="item-text">${item.text}</span>
@@ -63,37 +59,3 @@ document.addEventListener("click", (e) => {
     }
 })
 const update = document.getElementById('edit-button')
-
-
-// function createItem() {
-//     let readItems = 
-//         `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-//         <span class="item-text">${global.dbItems}</span>
-//         <div>
-//           <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-//           <button class="delete-me btn btn-danger btn-sm">Delete</button>
-//         </div>
-//       </li>`;
-//     itemList.insertAdjacentHTML('beforeend', readItems)
-// }
-
-// // paragraph.appendChild(text) = "testing"
-// // const body = document.getElementById('body')
-// // body.appendChild(paragraph)
-
-// // var para = document.createElement("p");
-// // var node = document.createTextNode("This is new.");
-// // para.appendChild(node);
-
-// // var element = document.getElementById("test");
-// // element.appendChild(para);
-
-// // `${items.map((item) => {
-// //     return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-// //     <span class="item-text">${item.text}</span>
-// //     <div>
-// //       <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-// //       <button class="delete-me btn btn-danger btn-sm">Delete</button>
-// //     </div>
-// //   </li>`
-// //   }).join('')}`
